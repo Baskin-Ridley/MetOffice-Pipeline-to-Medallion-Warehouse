f72732ef5f349299221ff0a9160e25be65132642
Baskin-Ridley <gabrielbaskinridley@gmail.com>
2026-04-30 18:15:44 +0100

    add fetch data function

---
+++ scripts/ingestion/ingest_met_office_to_bronze.py
@@ -0,0 +1,15 @@
+import os
+import requests
+import json
+from datetime import datetime
+from google.cloud import storage
+from dotenv import load_dotenv
+
+# loading secrets
+
+# functions
+
+def fetch_met_office_data(lat, lo):
+
+    pass
+
