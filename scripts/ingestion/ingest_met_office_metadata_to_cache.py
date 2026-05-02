a6e4ffc0ef555672fcc6b081ed379b9159693e11
Baskin-Ridley <gabrielbaskinridley@gmail.com>
2026-05-02 15:11:52 +0100

    updated to use apikey path

--- scripts/ingestion/ingest_met_office_metadata_to_cache.py
+++ scripts/ingestion/ingest_met_office_metadata_to_cache.py
@@ -7,7 +7,7 @@ from typing import Dict, Optional
 # configure
 SEEDS_FILE = "seeds/met_office_weather_stations_seed.csv"
 CACHE_DIR = "cache/met_office_metadata"
-API_KEY = os.getenv("MET_OFFICE_API_KEY")
+API_KEY = open(os.getenv("MET_OFFICE_API_KEY")).read().strip()
 HEADERS = {"apikey": API_KEY} # met office expects key to be in header
 BASE_URL = " https://data.hub.api.metoffice.gov.uk/observation-land/1/nearest"
 
