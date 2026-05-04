d0acb6fac76890fe65b82550dcf42980b84b1e3e
Baskin-Ridley <gabrielbaskinridley@gmail.com>
2026-05-04 17:44:04 +0100

    update test script to call observation ingestion

--- scripts/testing/test_call_metadata_pipeline.py
+++ scripts/testing/test_call_metadata_pipeline.py
@@ -6,4 +6,6 @@ subprocess.check_call(["python", "scripts/bronze/load_met_office_metadata_to_bro
 print("load_met_office_metadata_to_bronze.py complete")
 subprocess.check_call(["python", "scripts/silver/load_met_office_metadata_to_silver.py"])
 print("load_met_office_metadata_to_silver.py complete")
+subprocess.check_call(["python", "scripts/ingestion/ingest_met_office_land_observations_to_landed.py"])
+print("ingest_met_office_land_observations_to_landed.py complete")
 print("done")
