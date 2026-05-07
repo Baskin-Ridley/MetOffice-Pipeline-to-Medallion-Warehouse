f40c4dc7e849f626295c4147ed194c9d30146875
Baskin-Ridley <gabrielbaskinridley@gmail.com>
2026-05-07 17:53:59 +0100

    update test pipeline to include observations to silver

--- scripts/testing/test_call_metadata_pipeline.py
+++ scripts/testing/test_call_metadata_pipeline.py
@@ -8,4 +8,6 @@ subprocess.check_call(["python", "scripts/silver/load_met_office_metadata_to_sil
 print("load_met_office_metadata_to_silver.py complete")
 subprocess.check_call(["python", "scripts/ingestion/ingest_met_office_land_observations_to_landed.py"])
 print("ingest_met_office_land_observations_to_landed.py complete")
+subprocess.check_call(["python", "scripts/bronze/load_met_office_land_observations_to_bronze.py"])
+print("load_met_office_land_observations_to_bronze.py complete")
 print("done")
