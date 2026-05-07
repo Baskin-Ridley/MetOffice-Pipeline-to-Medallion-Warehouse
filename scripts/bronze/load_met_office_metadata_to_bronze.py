31511d3466b120b1706a3957503f69b83d364549
Baskin-Ridley <gabrielbaskinridley@gmail.com>
2026-05-07 17:28:43 +0100

    improved timestamp

--- scripts/bronze/load_met_office_metadata_to_bronze.py
+++ scripts/bronze/load_met_office_metadata_to_bronze.py
@@ -1,5 +1,5 @@
 from pyspark.sql import SparkSession
-from pyspark.sql.functions import current_timestamp, input_file_name, regexp_replace, regexp_extract, lit, sha2, concat_ws
+from pyspark.sql.functions import from_utc_timestamp, date_format, current_timestamp, input_file_name, regexp_replace, regexp_extract, lit, sha2, concat_ws
 import uuid
 from pathlib import Path
 from common.file_utils import get_latest_version_paths
@@ -33,7 +33,12 @@ def main():
     df_bronze = df \
         .withColumn("_source_file", regexp_replace(input_file_name(), "%20", " ")) \
         .withColumn("station_name", regexp_extract("_source_file", r"([^/]+)(?=\.json$)", 1)) \
-        .withColumn("_processed_at", date_format(current_timestamp(), "yyyy-MM-dd'T'HH:mm:ssXXX")) \
+        .withColumn("_processed_at", 
+            date_format(
+                from_utc_timestamp(current_timestamp(), "Europe/London"), 
+                "yyyy-MM-dd'T'HH:mm:ssXXX"
+            )
+        ) \
         .withColumn("_extraction_id", lit(extraction_id)) \
         .withColumn("_row_hash", sha2(concat_ws("||", *df.columns), 256))
 
