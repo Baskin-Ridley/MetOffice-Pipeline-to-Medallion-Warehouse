fecd76eb52f183dca9e03ca5e05893a54a1a3cfd
Baskin-Ridley <gabrielbaskinridley@gmail.com>
2026-05-31 13:54:24 +0100

    update to use spark Dataproc Cluster

--- dags/met_office_bronze.py
+++ dags/met_office_bronze.py
@@ -1,10 +1,11 @@
 from datetime import datetime, timedelta
-
 from airflow import DAG
-from airflow.operators.python import PythonOperator
+from airflow.providers.google.cloud.operators.dataproc import DataprocCreateBatchOperator
+from airflow.models import Variable
 
-from scripts.bronze.load_met_office_metadata_to_bronze import main as load_metadata_bronze_main
-from scripts.bronze.load_met_office_land_observations_to_bronze import main as load_observations_bronze_main
+PROJECT_ID = "noaa-medallion-warehouse"
+REGION = "europe-west2"
+BUCKET_NAME = Variable.get("datalake_bucket", "your-gcp-datalake-bucket")
 
 DEFAULT_ARGS = {
     "owner": "airflow",
@@ -25,14 +26,30 @@ with DAG(
     tags=["met-office", "bronze"],
 ) as dag:
 
-    metadata_bronze = PythonOperator(
+    metadata_bronze = DataprocCreateBatchOperator(
         task_id="load_met_office_metadata_to_bronze",
-        python_callable=load_metadata_bronze_main,
+        project_id=PROJECT_ID,
+        region=REGION,
+        batch_id="met-office-metadata-{{ ds_nodash }}-{{ task_instance.try_number }}",
+        batch={
+            "pyspark_batch": {
+                "main_python_file_uri": f"gs://{BUCKET_NAME}/scripts/bronze/load_met_office_metadata_to_bronze.py",
+                "python_file_uris": [f"gs://{BUCKET_NAME}/scripts/common/file_utils.py"]
+            }
+        }
     )
 
-    observations_bronze = PythonOperator(
+    observations_bronze = DataprocCreateBatchOperator(
         task_id="load_met_office_land_observations_to_bronze",
-        python_callable=load_observations_bronze_main,
+        project_id=PROJECT_ID,
+        region=REGION,
+        batch_id="met-office-obs-{{ ds_nodash }}-{{ task_instance.try_number }}",
+        batch={
+            "pyspark_batch": {
+                "main_python_file_uri": f"gs://{BUCKET_NAME}/scripts/bronze/load_met_office_land_observations_to_bronze.py",
+                "python_file_uris": [f"gs://{BUCKET_NAME}/scripts/common/file_utils.py"]
+            }
+        }
     )
 
     metadata_bronze >> observations_bronze
--- scripts/bronze/load_met_office_metadata_to_bronze.py
+++ scripts/bronze/load_met_office_metadata_to_bronze.py
@@ -2,7 +2,7 @@ from pyspark.sql import SparkSession
 from pyspark.sql.functions import from_utc_timestamp, date_format, current_timestamp, input_file_name, regexp_replace, regexp_extract, lit, sha2, concat_ws
 import uuid
 from upath import UPath
-from common.file_utils import get_latest_version_paths
+from file_utils import get_latest_version_paths
 import os
 from airflow.models import Variable
 
