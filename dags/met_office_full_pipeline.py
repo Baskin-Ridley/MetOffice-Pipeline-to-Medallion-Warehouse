05b96574787529392e112637760c41b8843f9437
Baskin-Ridley <gabrielbaskinridley@gmail.com>
2026-05-31 18:27:32 +0100

    fix call to debug function

--- dags/met_office_full_pipeline.py
+++ dags/met_office_full_pipeline.py
@@ -1,16 +1,19 @@
+import os
 import sys
 from datetime import datetime, timedelta
 
 from airflow import DAG
-from airflow.configuration import conf
 from airflow.models import Variable
+from airflow.operators.python import PythonOperator
 from airflow.operators.trigger_dagrun import TriggerDagRunOperator
 from airflow.providers.google.cloud.operators.dataproc import DataprocCreateBatchOperator
 
-DAGS_GCS_PATH = conf.get("core", "dags_folder").rstrip("/")
-if DAGS_GCS_PATH not in sys.path:
-    sys.path.append(DAGS_GCS_PATH)
+LOCAL_DAGS_PATH = "/home/airflow/gcs/dags"
+if LOCAL_DAGS_PATH not in sys.path:
+    sys.path.append(LOCAL_DAGS_PATH)
 
+GCS_BUCKET = os.environ.get("GCS_BUCKET")
+GCS_DAGS_PATH = f"{GCS_BUCKET}/dags"
 DATALAKE_BUCKET = Variable.get("datalake_bucket")
 
 def log_debug_info():
@@ -42,6 +45,11 @@ with DAG(
     tags=["met-office", "full-pipeline"],
 ) as dag:
 
+    debug_check = PythonOperator(
+        task_id="debug_environment_paths",
+        python_callable=log_debug_info,
+    )
+
     trigger_ingestion_layer = TriggerDagRunOperator(
         task_id="trigger_met_office_api_ingestion",
         trigger_dag_id="met_office_api_ingestion",
@@ -54,11 +62,11 @@ with DAG(
         batch_id="met-office-metadata-{{ ds_nodash }}-{{ task_instance.try_number }}",
         batch={
             "pyspark_batch": {
-                "main_python_file_uri": f"{DAGS_GCS_PATH}/scripts/bronze/load_met_office_metadata_to_bronze.py",
-                "python_file_uris": [f"{DAGS_GCS_PATH}/scripts/common/file_utils.py"],
+                "main_python_file_uri": f"{GCS_DAGS_PATH}/scripts/bronze/load_met_office_metadata_to_bronze.py",
+                "python_file_uris": [f"{GCS_DAGS_PATH}/scripts/common/file_utils.py"],
                 "args": [DATALAKE_BUCKET],
             }
         },
     )
 
-    trigger_ingestion_layer >> metadata_bronze
+    debug_check >> trigger_ingestion_layer >> metadata_bronze
